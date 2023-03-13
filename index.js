7bedcb7a0785eaeff193eb8be63a2c2d391c1773
Abdulakhad <57409902+iamdoc@users.noreply.github.com>
2023-03-13 23:27:22 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randomNumber1 = Math.floor((Math.random()*6)+1);
-var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+var randomDiceImage = "dice" + randomNumber1 + ".png";
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomDiceImage);
 
 var randomNumber2 = Math.floor((Math.random()*6)+1);
-var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomDiceImage2);
 
